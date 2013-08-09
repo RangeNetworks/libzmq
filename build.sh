3d0925aa670c768b3be35df16729f926fc9b4642
Michael Iedema <michael.iedema@rangenetworks.com>
2013-08-09 13:05:56 +0000

    - to avoid backport vs proper versioning hell, range now has a blessed version of libzmq to work on both 10.04 and 12.04
    git-svn-id: http://wush.net/svn/range/software/commercial/libzmq/trunk@6128 19bc5d8c-e614-43d4-8b26-e1612bc8e597

---
+++ build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+VERSION=3.2.2
+
+# git clone https://github.com/zeromq/cppzmq.git
+# zmq.hpp version 235803740753312576495301ebf5b8ed76407173
+
+sayAndDo () {
+	echo $@
+	eval $@
+	if [ $? -ne 0 ]
+	then
+		echo "ERROR: command failed!"
+		exit 1
+	fi
+}
+
+installIfMissing () {
+	dpkg -s $@ > /dev/null
+	if [ $? -ne 0 ]; then
+		echo " - oops, missing $@, installing"
+		sudo apt-get install $@
+	else
+		echo " - $@ ok"
+	fi
+	echo
+}
+
+if [ ! -f zeromq-$VERSION.tar.gz ]
+then
+	sayAndDo wget http://download.zeromq.org/zeromq-$VERSION.tar.gz
+fi
+
+if [ -d zeromq-$VERSION ]
+then
+	sayAndDo rm -rf zeromq-$VERSION
+fi
+
+rm range-libzmq*
+sayAndDo tar zxf zeromq-$VERSION.tar.gz
+sayAndDo mkdir zeromq-$VERSION/debian
+sayAndDo cp debian/* zeromq-$VERSION/debian/
+sayAndDo cd zeromq-$VERSION
+sayAndDo dpkg-buildpackage
